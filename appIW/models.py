8cf4cd414d4e3d946e89503565fd5c1e77d7e1ce
Roberto <Roberto@portatil>
2019-11-11 11:33:28 +0100

    Creación de los models

--- appIW/models.py
+++ appIW/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Provincia(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Estacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
+    nPistas = models.IntegerField()
+    nKilometros = models.IntegerField()
+
+class Pista(models.Model):
+    estacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    dificultad = models.CharField(max_length=10)
+    longitud = models.IntegerField()
