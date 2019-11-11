6e299bf1729b8a2ec6873da1cdf188aefe182843
Roberto <Roberto@portatil>
2019-11-11 11:52:57 +0100

    Corrección de errores en el models y migraciones hechas

--- appIW/models.py
+++ appIW/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Provincia(models.Model):
     nombre = models.CharField(max_length=50)
+
 
 class Estacion(models.Model):
     nombre = models.CharField(max_length=50)
     provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
     nPistas = models.IntegerField()
     nKilometros = models.IntegerField()
+
 
 class Pista(models.Model):
     estacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
