5597a48ef8f72a0518458629cc6c4578fc545791
Roberto <Roberto@portatil>
2019-11-11 11:16:55 +0100

    Funcionan perfectamente los HTML con los CSS. Ahora hay que meter la información necesaria

--- appIW/urls.py
+++ appIW/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('inicio', views.index),
+    path('', views.index),
 
 ]
--- appIW/views.py
+++ appIW/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'appIW/inicio.html')
+    return render(request, 'appIW/index.html')
