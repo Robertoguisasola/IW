e97fadba2a0fb208c2d7633788b0e82cd2463138
javit <javitxu.gonzalez@opendeusto.es>
2019-11-12 12:12:17 +0100

    admin creado

--- appIW/admin.py
+++ appIW/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Provincia, Estacion, Pista
+admin.site.register(Provincia)
+admin.site.register(Estacion)
+admin.site.register(Pista)
+
+
