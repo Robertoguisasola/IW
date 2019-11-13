5bfd10e9dacaed91826231170c82b7896880aeca
Roberto <Roberto@portatil>
2019-11-13 15:05:29 +0100

    Funcionan las cordilleras

--- appIW/admin.py
+++ appIW/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Cordillera, Estacion, Pista
+
 admin.site.register(Cordillera)
 admin.site.register(Estacion)
 admin.site.register(Pista)
--- appIW/views.py
+++ appIW/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Cordillera, Pista, Estacion
+from .models import Cordillera, Estacion
 
 
 # Create your views here.
 def index(request):
-    listaCordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
-    context = {'cordilleras': listaCordilleras}
-    #TODO enlazar estaciones a provincias
-    listaEstaciones = get_list_or_404(Estacion.objects.order_by('cordilleras'))
-    context2 = {'estaciones': listaEstaciones}
-    return render(request, 'appIW/index.html', context, context2)
+    cordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
+    context = {'listaCordilleras':cordilleras}
+    return render(request, 'appIW/index.HTML', context)
