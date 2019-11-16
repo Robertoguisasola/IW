9ff83e7339d0bb460b2677da4066c3d3207a2bee
Roberto <Roberto@portatil>
2019-11-16 20:20:06 +0100

    Tablas y más cosas añadidas, las views no he sabido como hacerlas bien y referenciarlas

--- appIW/views.py
+++ appIW/views.py
@@ -1,11 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Cordillera, Estacion
+from .models import Cordillera, Estacion, Pista
 
 
 # Create your views here.
+# view inicial
 def index(request):
     cordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
-    context = {'listaCordilleras':cordilleras}
+    context = {'listaCordilleras': cordilleras}
     return render(request, 'appIW/index.HTML', context)
+
+
+# Devuelve un listado de cordilleras
+def cordilleras(request):
+    cordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
+    context = {'listaCordilleras': cordilleras}
+    return render(request, 'appIW/cordilleras.HTML', context)
+
+
+# Devuelve los detalles de una cordillera
+def cordillera(request, cordillera_id):
+    cordillera = get_object_or_404(Cordillera, pk=cordillera_id)
+    context = {'cordillera': cordillera}
+    return render(request, 'appIW/cordillera.html', context)
+
+
+# Devuelve las estaciones de una cordillera
+def estaciones(request, cordillera_id):
+    cordillera = get_object_or_404(Cordillera, pk=cordillera_id)
+    estaciones = cordillera.estacion_set.all()
+    context = {'cordillera': cordillera, 'estaciones': estaciones}
+    return render(request, 'appIW/estaciones.html', context)
+
+
+# Devuelve los detalles de una estacion
+def estacion(request, estacion_id):
+    estacion = get_object_or_404(Estacion, pk=estacion_id)
+    context = {'estacion': estacion}
+    return render(request, 'appIW/estacion.HTML', context)
+
+
+# Devuelve las pistas de cada estación
+def pistas(request, estacion_id):
+    estacion = get_object_or_404(Estacion, pk=estacion_id)
+    pistas = estacion.pista_set.all()
+    context = {'estacion': estacion, 'pistas': pistas}
+    return render(request, 'appIW/pistas.html', context)
+
+
+# Devuelve los detalles de una pista
+def pista(request, pista_id):
+    pista = get_object_or_404(Pista, pk=pista_id)
+    context = {'pista': pista}
+    return render(request, 'appIW/pista.html', context)
