721667fab6546596fa262549ab131f7307ab12e3
Tucutufelo <anerlekue@opendeusto.es>
2019-11-16 21:18:11 +0100

    lista de estaciones, (no lo recorre por completo).

--- appIW/urls.py
+++ appIW/urls.py
@@ -1,10 +1,8 @@
-
 from django.urls import path
-
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:cordillera_id>/', views.estacionesLista, name='EstacionesLista'),
 
 ]
--- appIW/views.py
+++ appIW/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, get_list_or_404
-from .models import Cordillera, Estacion
+from .models import Cordillera
+from .models import Estacion
 
 
 # Create your views here.
@@ -9,3 +10,8 @@ def index(request):
     cordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
     context = {'listaCordilleras':cordilleras}
     return render(request, 'appIW/index.HTML', context)
+
+def estacionesLista(request, cordillera_id):
+    estaciones = get_list_or_404(Estacion, pk=cordillera_id)
+    context = {'listaEstacion': estaciones}
+    return render(request, 'appIW/EstacionesLista.html', context)
