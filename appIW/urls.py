3a3c114ca1bbb26f234647c0d5a5edf231d311a4
Roberto <Roberto@portatil>
2019-11-17 17:51:22 +0100

    insercción de las urls y primera modificación del index.HTML

--- appIW/urls.py
+++ appIW/urls.py
@@ -1,10 +1,16 @@
-
-from django.urls import path
-
+from django.urls import path, reverse_lazy
+from django.contrib import admin
+from django.views.generic import RedirectView
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='indice'),
-
+    path('home/', views.index, name='indice'),
+    path('/', views.cordilleras, name ='cordilleras'),
+    path('<int:cordillera_id>/', views.cordillera, name='cordillera'),
+    path('<int:cordillera_id>/', views.estaciones, name='estaciones'),
+    path('<int:estacion_id>/', views.estacion, name='estacion'),
+    path('<int:estacion_id>/', views.pistas, name='pistas'),
+    path('<int:pista_id>/', views.pista, name='pista'),
+    path('admin/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='admin')
 ]
