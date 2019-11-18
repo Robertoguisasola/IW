a9b014b4cecb07b84764a5aaeb8e3b79036c667a
Tucutufelo <anerlekue@opendeusto.es>
2019-11-18 16:56:02 +0100

    URls

--- IW/urls.py
+++ IW/urls.py
@@ -19,6 +19,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url=reverse_lazy('admin:index')), name='adminv'),
     path('', include('appIW.urls')),
 ]
