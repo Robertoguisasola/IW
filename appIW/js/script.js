7d8cfd5fa20cc5d755b69b770ec7d11dbb76fc58
javit <javitxu.gonzalez@opendeusto.es>
2019-11-28 10:11:22 +0100

    estacion

---
+++ appIW/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("#cordillera1").click(function(e){
+    $("#cordillera1body").slideUp();
+    })
+});
+$(document).ready(function(){
+
+    $("#cordillera1").dblclick(function(e){
+    $("#cordillera1body").slideDown();
+    })
+});
