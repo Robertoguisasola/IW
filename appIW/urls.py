2ddc91b421cf6af387d9a248668abdc640f4865c
Robertoguisasola <Roberto@portatil>
2019-12-04 13:55:51 +0100

    Saca los enlaces a las webs oficiales de las estaciones de esquí

--- appIW/urls.py
+++ appIW/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 
 urlpatterns = [
-    path('index', views.index, name='indice'),
-    path('cordillera/<int:cordillera_id>/', views.cordillera, name='cordillera'),
-    path('estacion/<int:estacion_id>/', views.estacion, name='estacion'),
-    path('pista/<int:pista_id>/', views.pista, name='pista'),
+    path('index', views.index, name='indice'), #portada de la web con dos estaciones por localización
+    path('cordillera/<int:cordillera_id>/', views.cordillera, name='cordillera'), #detalles de una cordillera determinada
+    path('estacion/<int:estacion_id>/', views.estacion, name='estacion'), #detalles de una estación de esquí
+    path('pista/<int:pista_id>/', views.pista, name='pista'), #detalles de una pista
     path('admin/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='admin')
 ]
