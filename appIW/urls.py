52702de10621b45809cdaf14e44e3969162f55f1
Robertoguisasola <Roberto@portatil>
2019-12-15 12:52:10 +0100

    Funciona todo como debe

--- appIW/urls.py
+++ appIW/urls.py
@@ -6,8 +6,13 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='indice'), #portada de la web con dos estaciones por localización
+    path('cordilleras', views.cordilleras, name='cordilleras'), # todas las cordilleras y sus estaciones
     path('cordillera/<int:cordillera_id>/', views.cordillera, name='cordillera'), #detalles de una cordillera determinada
+    path('estaciones', views.estaciones, name='estaciones'), #todas las estaciones
     path('estacion/<int:estacion_id>/', views.estacion, name='estacion'), #detalles de una estación de esquí
+    path('pistas', views.pistas, name='pistas'), #todas las pistas por estacion
     path('pista/<int:pista_id>/', views.pista, name='pista'), #detalles de una pista
+    path('contacto/', views.contacto, name='contacto'),
     path('admin/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='admin')
+
 ]
--- appIW/views.py
+++ appIW/views.py
@@ -19,12 +19,27 @@ def index(request):
     return render(request, 'appIW/index.HTML', context)
 
 
+# Devuelve las cordilleras
+def cordilleras(request):
+    cordilleras = get_list_or_404(Cordillera.objects.order_by('nombre'))
+    estaciones = get_list_or_404(Estacion.objects.order_by('-nKilometrosTotales'))
+    context = {'listaCordilleras': cordilleras, 'listaEstaciones': estaciones}
+    return render(request, 'appIW/cordilleras.html', context)
+
+
 # Devuelve los detalles de una cordillera
 def cordillera(request, cordillera_id):
     cordillera = get_object_or_404(Cordillera, pk=cordillera_id)
     estaciones = cordillera.estacion_set.all()
     context = {'cordillera': cordillera, 'estaciones': estaciones}
     return render(request, 'appIW/cordillera.html', context)
+
+
+# Devuelve los detalles de una estacion
+def estaciones(request):
+    estaciones = get_list_or_404(Estacion.objects.order_by('-nKilometrosTotales'))
+    context = {'estaciones': estaciones}
+    return render(request, 'appIW/estaciones.html', context)
 
 
 # Devuelve los detalles de una estacion
@@ -35,8 +50,20 @@ def estacion(request, estacion_id):
     return render(request, 'appIW/estacion.html', context)
 
 
+# Devuelve todas las pistas por cada estación
+def pistas(request):
+    estaciones = get_list_or_404(Estacion.objects.order_by('-nKilometrosTotales'))
+    pistas = get_list_or_404(Pista.objects.order_by('nombre'))
+    context = {'listaEstaciones': estaciones, 'listaPistas': pistas}
+    return render(request, 'appIW/pistas.html', context)
+
+
 # Devuelve los detalles de una pista
 def pista(request, pista_id):
     pista = get_object_or_404(Pista, pk=pista_id)
     context = {'pista': pista}
     return render(request, 'appIW/pista.html', context)
+
+
+def contacto(request):
+    return render(request, 'appIW/contacto.html')
