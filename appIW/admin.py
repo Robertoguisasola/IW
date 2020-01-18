4e1d25eac143dec54583ddf5b8b3c1e3b5a692ec
Robertoguisasola <44500206+Robertoguisasola@users.noreply.github.com>
2020-01-18 20:36:48 +0100

    Personalización de la aplicación de administrador

--- appIW/admin.py
+++ appIW/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Cordillera, Estacion, Pista
+
 # Register your models here.
 
 admin.site.register(Cordillera)
-admin.site.register(Estacion)
-admin.site.register(Pista)
+
+
+class EstacionAdmin(admin.ModelAdmin):
+    fields = ['nombre', 'cordillera', 'enlacePagina', 'nPistasTotales', 'nKilometrosTotales']
+
+
+class PistaAdmin(admin.ModelAdmin):
+    fields = ['estacion', 'nombre', 'dificultad', 'longitud', 'abierta']
+
+
+admin.site.register(Estacion, EstacionAdmin)
+admin.site.register(Pista, PistaAdmin)
